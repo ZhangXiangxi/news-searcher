8c2afc535b3699a8f514c8e1927c91f56e367357
ZhangXiangxi <zxx_1996@qq.com>
2018-06-09 20:39:10 +0800

    The write and read from MySQL database for word_table

--- src/main/java/WordDAO.java
+++ src/main/java/WordDAO.java
@@ -1,8 +1,82 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by Xiangxi on 2018/6/9.
  * Contact him on xiangxi.zhang.cs@gmail.com
  */
 // insert and search a word with MySQL
 public class WordDAO {
-    // TODO:
+    Connection connection;
+    StringBuilder builder;
+    public WordDAO() {
+        connection = new DBPool().getConn();
+        builder = new StringBuilder();
+    }
+    public void insertWord(int id, String word) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(insertSqlBuilder(word, id));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public String searchWordWithID(int id) {
+        String sqlString = searchWordByIdSqlBuilder(id);
+        String result = "";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlString);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean hasNext = resultSet.next();
+            assert(hasNext);
+            result =  resultSet.getString("word");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        assert(!result.equals(""));
+        return result;
+    }
+
+    public int searchIDWithWord(String word) {
+        // the word should be pure-alphabetic and in lower case
+        String sqlString = searchIDWithWordBuilder(word);
+        int result = -1;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlString);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            boolean hasNext = resultSet.next();
+            assert(hasNext);
+            result =  resultSet.getInt("word_id");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        assert(result != -1);
+        return result;
+    }
+
+    public String insertSqlBuilder(String word, int id) {
+        builder.setLength(0);
+        builder.append("INSERT INTO word_table VALUE (");
+        builder.append(id);
+        builder.append(", \"");
+        builder.append(word);
+        builder.append("\");");
+        return builder.toString();
+    }
+    public String searchWordByIdSqlBuilder(int id) {
+        builder.setLength(0);
+        builder.append("SELECT * FROM word_table WHERE word_id = ");
+        builder.append(id);
+        builder.append(";");
+        return builder.toString();
+    }
+    public String searchIDWithWordBuilder(String word) {
+        builder.setLength(0);
+        builder.append("SELECT * FROM word_table WHERE word = \"");
+        builder.append(word);
+        builder.append("\";");
+        return builder.toString();
+    }
 }
---
+++ src/test/java/WordDAOTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Xiangxi on 2018/6/9.
+ * Contact him on xiangxi.zhang.cs@gmail.com
+ */
+public class WordDAOTest {
+    public WordDAO wordDAO;
+
+    @Before
+    public void setUp() {
+        wordDAO = new WordDAO();
+    }
+    @Test
+    public void testWrite() {
+        System.out.println(wordDAO.insertSqlBuilder("Test", 4));
+        wordDAO.insertWord(4, "Test");
+    }
+    @Test
+    public void testSearchByID() {
+        System.out.println(wordDAO.searchWordWithID(4));
+    }
+    @Test
+    public void testSearchByWord() {
+        System.out.println(wordDAO.searchIDWithWord("Test"));
+    }
+}
