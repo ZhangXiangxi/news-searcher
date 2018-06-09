38aa532ab5b2b50ba3be6cacb5b1e16776fe49ab
ZhangXiangxi <zxx_1996@qq.com>
2018-06-09 21:37:18 +0800

    Add a stop word list and change the alphabetic filter

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -16,6 +16,19 @@ public class Dictionary {
     public ArrayList<WordEntry> dictionary;
     public Trie<String, Integer> wordCount;
     public WordDAO wordDAO;
+    public static final String[] STOP_WORDS = { "a", "about", "above", "after", "again", "against", "all", "am", "an",
+            "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both",
+            "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from",
+            "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
+            "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into",
+            "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on",
+            "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she",
+            "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their",
+            "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're",
+            "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd",
+            "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while",
+            "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've",
+            "your", "yours", "yourself", "yourselves", "s", "t", "ll", "ve", "m", "d", "re"};
     boolean isReady = false;
     public Dictionary() {
         dictionary = new ArrayList<>();
@@ -63,24 +76,27 @@ public class Dictionary {
         boolean readState = false; // mark whether the automata is reading words or not.
         StringBuilder builder = new StringBuilder();
         for(char c : content.toCharArray()) {
-            if (!readState && !Character.isAlphabetic(c))
+            if (!readState && !isEnglishLetter(c))
                 continue;
-            if (!readState && Character.isAlphabetic(c)) {
+            if (!readState && isEnglishLetter(c)) {
                 builder.setLength(0);
                 readState = true;
                 builder.append(Character.toLowerCase(c));
                 continue;
             }
-            if (readState && Character.isAlphabetic(c)) {
+            if (readState && isEnglishLetter(c)) {
                 builder.append(Character.toLowerCase(c));
                 continue;
             }
-            if (readState && !Character.isAlphabetic(c)) {
+            if (readState && !isEnglishLetter(c)) {
                 result.add(builder.toString());
                 readState = false;
             }
         }
         return result;
+    }
+    private boolean isEnglishLetter(char c) {
+        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
     }
 
     public class WordEntry implements Comparable{
